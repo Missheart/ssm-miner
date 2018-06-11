a71703615849e685ad42e06e33b3c357c980aec7
CDlihang <1332675356@qq.com>
2018-06-11 16:56:31 +0800

    updated

--- src/main/java/home/controller/ManagerController.java
+++ src/main/java/home/controller/ManagerController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * 〈一句话功能简述〉<br> 
